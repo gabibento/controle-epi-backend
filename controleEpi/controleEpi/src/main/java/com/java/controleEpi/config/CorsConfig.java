14416878a977ffa6a697c5bd5a06e554b90167a7
Gabriella <gabriella.mabento@gmail.com>
2025-06-18 10:39:37 -0300

    fix: alteração na configuração de CORS

--- controleEpi/controleEpi/src/main/java/com/java/controleEpi/config/CorsConfig.java
+++ controleEpi/controleEpi/src/main/java/com/java/controleEpi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
