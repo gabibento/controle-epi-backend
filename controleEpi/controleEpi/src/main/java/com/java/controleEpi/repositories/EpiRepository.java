760e2f0dbb96bbc7ff88d6788a45f2942f8226c5
TAINÁ ESTEFANI MARTINS <taina_e_martins@estudante.sesisenai.org.br>
2025-06-17 21:30:11 -0300

    feat: adicionado método findAll em EpiRepository, EpiService, EpiController

--- controleEpi/controleEpi/src/main/java/com/java/controleEpi/repositories/EpiRepository.java
+++ controleEpi/controleEpi/src/main/java/com/java/controleEpi/repositories/EpiRepository.java
@@ -3,5 +3,9 @@ package com.java.controleEpi.repositories;
 import com.java.controleEpi.entities.Epi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EpiRepository extends JpaRepository<Epi, Long> {
+    Optional<List<Epi>> findByName(String name);
 }
