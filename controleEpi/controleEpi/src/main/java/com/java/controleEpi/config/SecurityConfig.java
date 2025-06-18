3e896c5ff5bcb0e55aba88240cc08008964b8089
Gabriella <gabriella.mabento@gmail.com>
2025-06-18 10:59:55 -0300

    fix: alteração de configuração de autorização

--- controleEpi/controleEpi/src/main/java/com/java/controleEpi/config/SecurityConfig.java
+++ controleEpi/controleEpi/src/main/java/com/java/controleEpi/config/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
